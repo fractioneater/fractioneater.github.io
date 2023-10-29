3ab9f0ea7d53106dcde0cbc285cbbcdeb7d35291
Lewis Johnson <fractioneater@gmail.com>
2023-10-29 08:13:44 -0400

    Fix syntax highlighter issues

--- flicker/shiki.js
+++ flicker/shiki.js
@@ -1,3 +1,5 @@
+const code = document.getElementById("example-code-src").textContent;
+
 const response = await fetch("/flicker.tmLanguage.json");
 const flickerGrammar = await response.json();
 
@@ -9,25 +11,8 @@ shiki.getHighlighter({
       scopeName: "source.flicker",
       grammar: flickerGrammar
     }
-  ],
+  ]
 }).then(highlighter => {
-  const code = highlighter.codeToHtml(
-`var i = 4
-
-if i == 3
-  print "three"
-
-class Test
-  init(+property)
-    print "initializing instance"
-
-  getProperty()
-    return this.property
-
-print Test(49.202).getProperty()
-
-# output:
-# initializing instance
-# 49.202`, { lang: "flicker" })
-  document.getElementById("example-code").innerHTML = code;
+  const html = highlighter.codeToHtml(code, { lang: "flicker" })
+  document.getElementById("example-code").innerHTML = html;
 });
