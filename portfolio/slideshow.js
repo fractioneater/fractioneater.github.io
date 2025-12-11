db98f84ec67127474c5d9a0d1b5ffe015c5a8d72
fractioneater <fractioneater@gmail.com>
2025-12-11 17:29:59 -0500

    A start

---
+++ portfolio/slideshow.js
@@ -0,0 +1,37 @@
+let slideIndex = 0;
+window.onload = () =>
+  showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n - 1);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("image-slide");
+  console.log(slides);
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+
+  if (n >= slides.length)
+    slideIndex = 0
+
+  if (n < 0)
+    slideIndex = slides.length - 1
+
+  for (i = 0; i < slides.length; i++)
+    slides[i].style.display = "none";
+
+  for (i = 0; i < dots.length; i++)
+    dots[i].className = dots[i].className.replace(" active", "");
+
+  slides[slideIndex].style.display = "flex";
+  dots[slideIndex].className += " active";
+  captionText.innerHTML = dots[slideIndex].alt;
+}
